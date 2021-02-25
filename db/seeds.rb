80dc12a1e11c2f74288c9e8fcfe8a233912bec11
Sam McCarthy <samrmccarthy6@gmail.com>
2021-02-25 14:29:16 +1100

    Added ingredients through seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.delete_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_tings = open(url).read
+ingredients_array = JSON.parse(ingredients_tings)["drinks"]
+
+ingredients_array.each do |ingredient_hash|
+  Ingredient.create!(name: ingredient_hash["strIngredient1"])
+end
+
+# puts "Creating restaurants..."
+# dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+# pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+# [ dishoom, pizza_east ].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+# puts "Finished!"
